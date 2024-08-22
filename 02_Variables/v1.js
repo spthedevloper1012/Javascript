b4b88d611ecb53b4d01776a0a3625cd1ce47b218
Smit Patel1012 <108466745+spthedevloper1012@users.noreply.github.com>
2024-08-22 06:56:58 +0000

    fourth commit

--- 02_Variables/v1.js
+++ 02_Variables/v1.js
@@ -2,5 +2,11 @@ const accountId = 454545;
 let accountEmail = "smir@gamil.com";
 var accountPassword = "12345";
 accountCity = "Jaipur";
+let accountState;
+
+accountEmail = "hp@gmail.com"
+accountPassword = "212212";
+accountCity = "bengaluru";
 
 console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
