bbf66a71e78d47786644ac4ebf09d26ccaeced5e
Smit Patel1012 <108466745+spthedevloper1012@users.noreply.github.com>
2024-08-22 07:15:33 +0000

    datatypes

---
+++ d1.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log(3 + 3) 
+
+let name = "smir"
+let age = 19
+let isLoggedIn = true
+let state;
+
+/* this all are primitive datatypes 
+number 
+bigint
+string
+boolean
+null
+undefined
+symbol => unique */
+
+// Object
+
+console.log(typeof age);
+
