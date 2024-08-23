4a269e3b5f22a71693548e9b675f55ed1c00fc76
Smit Patel1012 <108466745+spthedevloper1012@users.noreply.github.com>
2024-08-23 04:27:51 +0000

    Conversion

---
+++ 04_Conversion/c1.js
@@ -0,0 +1,27 @@
+let score = "33aaa"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+//  "33" => 33
+// "33abc" => NaN
+
+let isLoggedIN = "smair";
+
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+// console.log(booleanIsLoggedIn)
+
+
+let SomeNumber = 33
+
+let stringNumber = String (SomeNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+console.log("hello")
